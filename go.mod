a539b2d5863a3653d3a6488b6567839676a87825
Shinichi Ishimura <sishimura@seqsense.com>
2020-07-11 07:07:51 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kamatama41/renovate-test
+
+go 1.14
+
+require github.com/aws/aws-sdk-go v1.33.1
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+func main() {
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("ap-northeast-1"),
+	}))
+	cli := s3.New(sess)
+	out, err := cli.ListBuckets(&s3.ListBucketsInput{})
+	if err != nil {
+		panic(err)
+	}
+	for _, b := range out.Buckets {
+		println(*b.Name)
+	}
+}
